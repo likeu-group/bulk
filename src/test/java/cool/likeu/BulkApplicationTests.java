5d3660e842b68d37f72bfa66cde95c62cc49683c
GangHuo <993610942@qq.com>
2020-09-05 06:45:15 +0800

    1. completion configuration 2. add checkstyle, but dit not add checkstyle plugin

---
+++ src/test/java/cool/likeu/BulkApplicationTests.java
@@ -0,0 +1,14 @@
+package cool.likeu;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
