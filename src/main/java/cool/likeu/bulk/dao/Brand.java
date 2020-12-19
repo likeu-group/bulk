9203afeee504395f560e0197a441bc388611f426
GangHuo <993610942@qq.com>
2020-12-19 16:51:40 +0800

    配置文件中新增了table-prefix和id-type的配置，并将数据库里对应的TableName与IdType注解移除掉

--- src/main/java/cool/likeu/bulk/dao/Brand.java
+++ src/main/java/cool/likeu/bulk/dao/Brand.java
@@ -3,17 +3,14 @@ package cool.likeu.bulk.dao;
 import java.io.Serializable;
 import java.util.Date;
 
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
-@TableName("bulk_brand")
 public class Brand implements Serializable {
 
-	@TableId(value = "brand_id", type = IdType.AUTO)
+	@TableId(value = "brand_id")
 	private Long id;
 
 	@TableField("brand_name")
--- src/main/java/cool/likeu/bulk/dao/Category.java
+++ src/main/java/cool/likeu/bulk/dao/Category.java
@@ -3,17 +3,14 @@ package cool.likeu.bulk.dao;
 import java.io.Serializable;
 import java.util.Date;
 
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
-@TableName("bulk_category")
 public class Category implements Serializable {
 
-	@TableId(value = "category_id", type = IdType.AUTO)
+	@TableId(value = "category_id")
 	private Long id;
 
 	@TableField("category_name")
